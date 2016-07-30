d002728027db9e98856f136ba1615c3e693b80cb
Steven Mortimer <reportmort@gmail.com>
2016-07-29 22:23:00 -0400

    Add repos for code page

---
+++ js/data/load_members.js
@@ -0,0 +1,22 @@
+$(function() { loadMemberData([
+  {
+    "received_events_url": "https://api.github.com/users/ReportMort/received_events",
+    "following_url": "https://api.github.com/users/ReportMort/following{/other_user}",
+    "followers_url": "https://api.github.com/users/ReportMort/followers",
+    "html_url": "https://github.com/ReportMort",
+    "url": "https://api.github.com/users/ReportMort",
+    "gravatar_id": "",
+    "avatar_url": "https://avatars.githubusercontent.com/u/5299737?v=3",
+    "id": 5299737,
+    "type": "User",
+    "site_admin": false,
+    "login": "ReportMort",
+    "gists_url": "https://api.github.com/users/ReportMort/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/ReportMort/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/ReportMort/subscriptions",
+    "organizations_url": "https://api.github.com/users/ReportMort/orgs",
+    "repos_url": "https://api.github.com/users/ReportMort/repos",
+    "events_url": "https://api.github.com/users/ReportMort/events{/privacy}"
+  }
+]
+); })
