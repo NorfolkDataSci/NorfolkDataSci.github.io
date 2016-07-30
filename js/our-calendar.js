fec02e86ef374a0ab2b9eb20b59cb3484a393166
Steven Mortimer <reportmort@gmail.com>
2016-07-30 10:52:08 -0400

    Fiddling Around with FullCalendar

---
+++ js/our-calendar.js
@@ -0,0 +1,13 @@
+<script type='text/javascript'>
+
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        googleCalendarApiKey: 'AIzaSyBsN5BeKS0RyzBSlyzyKzmJPr15v8_yBak',
+        events: {
+            googleCalendarId: 'norfolkdatasci@gmail.com',
+            className: 'gcal-event' // an option!
+        }
+    });
+});
+
+</script>
